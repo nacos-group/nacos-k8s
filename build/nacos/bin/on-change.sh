6eb9f9dfd86ba1573908f7798d4868271f10a877
zhanglong <huangmnlove@163.com>
2018-11-25 18:23:42 +0800

    scale test

--- build/nacos/bin/on-change.sh
+++ build/nacos/bin/on-change.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+
 while read -ra LINE; do
     PEERS=("${PEERS[@]}" $LINE)
 done
 
+echo "" > "${CLUSTER_CONF}"
 
-echo ${PEERS}
+if [ ${#PEERS[@]} -eq 1 ]; then
+
+    echo "${PEERS[0]}:$NACOS_SERVER_PORT" > "${CLUSTER_CONF}"
+    exit
+fi
+for peer in "${PEERS[@]}"; do
+ echo "${peer}:$NACOS_SERVER_PORT" >> "${CLUSTER_CONF}"
+done
+
+echo "on change write peers:"${PEERS}
+curl -X PUT 'http://localhost:8848/nacos/v1/ns/instance?serviceName=nacos.naming.serviceName&ip=20.18.7.2&port=8888'
+
+curl -X GET 'http://localhost:8848/nacos/v1/ns/instances?serviceName=nacos.naming.serviceName' | python -m json.tool
+
+curl -X GET "http://localhost:8848/nacos/v1/ns/raft/state" | python -m json.tool
---
+++ build/nacos/bin/on-start.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+
+while read -ra LINE; do
+    PEERS=("${PEERS[@]}" $LINE)
+done
+
+echo "" > "${CLUSTER_CONF}"
+
+
+for peer in "${PEERS[@]}"; do
+ echo "${peer}:$NACOS_SERVER_PORT" >> "${CLUSTER_CONF}"
+done
+
+export DEFAULT_SEARCH_LOCATIONS="classpath:/,classpath:/config/,file:./,file:./config/"
+export CUSTOM_SEARCH_LOCATIONS=${DEFAULT_SEARCH_LOCATIONS},file:${BASE_DIR}/conf/
+
+
+
+
+#===========================================================================================
+# JVM Configuration
+#===========================================================================================
+
+if [[ "${MODE}" == "standalone" ]]; then
+
+    JAVA_OPT="${JAVA_OPT} -Xms512m -Xmx512m -Xmn256m"
+    JAVA_OPT="${JAVA_OPT} -Dnacos.standalone=true"
+else
+
+  JAVA_OPT="${JAVA_OPT} -server -Xms2g -Xmx2g -Xmn1g -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=320m"
+  JAVA_OPT="${JAVA_OPT} -XX:-OmitStackTraceInFastThrow -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${BASE_DIR}/logs/java_heapdump.hprof"
+  JAVA_OPT="${JAVA_OPT} -XX:-UseLargePages"
+
+
+fi
+
+JAVA_MAJOR_VERSION=$($JAVA -version 2>&1 | sed -E -n 's/.* version "([0-9]*).*$/\1/p')
+if [[ "$JAVA_MAJOR_VERSION" -ge "9" ]] ; then
+  JAVA_OPT="${JAVA_OPT} -Xlog:gc*:file=${BASE_DIR}/logs/nacos_gc.log:time,tags:filecount=10,filesize=102400"
+else
+  JAVA_OPT="${JAVA_OPT} -Xloggc:${BASE_DIR}/logs/nacos_gc.log -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=100M"
+fi
+
+if [[ "${PREFER_HOST_MODE}" == "hostname" ]]; then
+    JAVA_OPT="${JAVA_OPT} -Dnacos.preferHostnameOverIp=true"
+fi
+JAVA_OPT="${JAVA_OPT} -Dnacos.home=${BASE_DIR}"
+JAVA_OPT="${JAVA_OPT} -jar ${BASE_DIR}/target/nacos-server.jar"
+JAVA_OPT="${JAVA_OPT} ${JAVA_OPT_EXT}"
+JAVA_OPT="${JAVA_OPT} --spring.config.location=${CUSTOM_SEARCH_LOCATIONS}"
+JAVA_OPT="${JAVA_OPT} --logging.config=${BASE_DIR}/conf/nacos-logback.xml"
+
+echo "nacos is starting,you can check the ${BASE_DIR}/logs/start.out"
+echo "$JAVA ${JAVA_OPT}" > ${BASE_DIR}/logs/start.out 2>&1 &
+nohup $JAVA ${JAVA_OPT} > ${BASE_DIR}/logs/start.out 2>&1 &
+
