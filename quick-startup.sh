d7808aba9442c1701b8eb1569e2709c7db5e1253
Naah <nayan3480232@163.com>
2019-04-19 17:54:13 +0800

    redundant words
    redundant words

--- quick-startup.sh
+++ quick-startup.sh
@@ -7,4 +7,4 @@ echo "mysql slave startup"
 kubectl create -f ./deploy/mysql/mysql-slave-local.yaml
 
 echo "nacos quick startup"
-kubectl create -f ./deploy/nacos/nacos-quick-start.yamlemptyDirs will possibly result in a loss of data
+kubectl create -f ./deploy/nacos/nacos-quick-start.yaml
