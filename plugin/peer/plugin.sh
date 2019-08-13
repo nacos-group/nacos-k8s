1c3c12ea72952eb1c9024f7c2156941a74834325
zhanglong <huangmnlove@163.com>
2019-08-13 22:29:06 +0800

    Fix word misspelling

--- plugin/peer/plugin.sh
+++ plugin/peer/plugin.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 PEER_FINDER_DIR="/home/nacos/plugins/peer-finder"
-cp /etc/hosts ~/hosts.new && sed -i 's/cluster.local./cluster.local/g' ~/hosts.new && cp -f ~/hosts.new /etc/hosts
+cp /etc/hosts ~/hosts.new && sed -i 's/cluster.local. /cluster.local /g' ~/hosts.new && cp -f ~/hosts.new /etc/hosts
 cd ${PEER_FINDER_DIR}
 ./peer-finder -on-start=${PEER_FINDER_DIR}/on-start.sh -on-change=${PEER_FINDER_DIR}/on-change.sh -service=${SERVICE_NAME} &
