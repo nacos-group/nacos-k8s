ac0d2e58cb5f4efdb7f0edc77bf68bbbdac10cc1
zhanglong <huangmnlove@163.com>
2020-02-22 14:14:20 +0800

    修改peerfinder插件,增加1.0版本镜像 #68

--- plugin/peer/plugin.sh
+++ plugin/peer/plugin.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 PEER_FINDER_DIR="/home/nacos/plugins/peer-finder"
-cp /etc/hosts ~/hosts.new && sed -i 's/cluster.local.[][]*/cluster.local /g' ~/hosts.new && cp -f ~/hosts.new /etc/hosts
-cd ${PEER_FINDER_DIR}
-./peer-finder -on-start=${PEER_FINDER_DIR}/on-start.sh -on-change=${PEER_FINDER_DIR}/on-change.sh -service=${SERVICE_NAME} &
+cd ${PEER_FINDER_DIR} ./peer-finder -on-start=${PEER_FINDER_DIR}/on-start.sh -on-change=${PEER_FINDER_DIR}/on-change.sh -service=${SERVICE_NAME} -domain=${DOMAIN_NAME} || exit &
