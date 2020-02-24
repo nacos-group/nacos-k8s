1c082506fa65a1b69e7a07ed6c090771d3ec0dbb
zhanglong <huangmnlove@163.com>
2020-02-24 18:20:57 +0800

    修复 plugin.sh 脚本执行问题

--- plugin/peer/plugin.sh
+++ plugin/peer/plugin.sh
@@ -2,4 +2,4 @@
 PEER_FINDER_DIR="/home/nacos/plugins/peer-finder"
 cp /etc/hosts ~/hosts.new && sed -i 's/cluster.local.[][]*/cluster.local /g' ~/hosts.new && cp -f ~/hosts.new /etc/hosts
 
-cd ${PEER_FINDER_DIR} ./peer-finder -on-start=${PEER_FINDER_DIR}/on-start.sh -on-change=${PEER_FINDER_DIR}/on-change.sh -service=${SERVICE_NAME} -domain=${DOMAIN_NAME} || exit &
+cd ${PEER_FINDER_DIR} && ./peer-finder -on-start=${PEER_FINDER_DIR}/on-start.sh -on-change=${PEER_FINDER_DIR}/on-change.sh -service=${SERVICE_NAME} -domain=${DOMAIN_NAME} || exit &
