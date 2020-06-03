6c5c6da51fca8573094ced64181dc946f361901a
wecantstop <13520773230@163.com>
2020-06-03 23:51:52 +0800

    Change NACOS_SERVER_PORT to NACOS_APPLICATION_PORT, Add DEFAULT PORT 8848

--- plugin/peer/on-change.sh
+++ plugin/peer/on-change.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 
+NACOS_APPLICATION_PORT=${NACOS_APPLICATION_PORT:-8848}
+
 while read -ra LINE; do
     PEERS=("${PEERS[@]}" $LINE)
 done
@@ -9,11 +11,11 @@ echo "" > "${CLUSTER_CONF}"
 
 if [ ${#PEERS[@]} -eq 1 ]; then
 
-    echo "${PEERS[0]}:$NACOS_SERVER_PORT" > "${CLUSTER_CONF}"
+    echo "${PEERS[0]}:$NACOS_APPLICATION_PORT" > "${CLUSTER_CONF}"
     exit
 fi
 for peer in "${PEERS[@]}"; do
- echo "${peer}:$NACOS_SERVER_PORT" >> "${CLUSTER_CONF}"
+ echo "${peer}:$NACOS_APPLICATION_PORT" >> "${CLUSTER_CONF}"
 done
 
 echo "on change write peers:"${PEERS}
--- plugin/peer/on-start.sh
+++ plugin/peer/on-start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 
+NACOS_APPLICATION_PORT=${NACOS_APPLICATION_PORT:-8848}
+
 while read -ra LINE; do
     PEERS=("${PEERS[@]}" $LINE)
 done
@@ -9,7 +11,7 @@ echo "" > "${CLUSTER_CONF}"
 
 
 for peer in "${PEERS[@]}"; do
- echo "${peer}:$NACOS_SERVER_PORT" >> "${CLUSTER_CONF}"
+ echo "${peer}:$NACOS_APPLICATION_PORT" >> "${CLUSTER_CONF}"
 done
 
 
