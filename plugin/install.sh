fe5cdd7403071e021e41c0ea640b809dd98fa4d4
zhanglong <huangmnlove@163.com>
2020-07-19 20:48:13 +0800

    Fix issue #140

--- plugin/install.sh
+++ plugin/install.sh
@@ -4,9 +4,9 @@ WORKDIR_VOLUME="/home/nacos/plugins/peer-finder"
 echo installing config scripts into "${WORKDIR_VOLUME}"
 
 mkdir -p "${WORKDIR_VOLUME}"
-cp /on-start.sh "${WORKDIR_VOLUME}"/
-cp /on-change.sh "${WORKDIR_VOLUME}"/
-cp /plugin.sh "${WORKDIR_VOLUME}"/
-cp /peer-finder "${WORKDIR_VOLUME}"/
+cp -f /on-start.sh "${WORKDIR_VOLUME}"/
+cp -f /on-change.sh "${WORKDIR_VOLUME}"/
+cp -f /plugin.sh "${WORKDIR_VOLUME}"/
+cp -f /peer-finder "${WORKDIR_VOLUME}"/
 
 
