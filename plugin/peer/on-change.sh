3c5b4ed4a6cc15ab05e4d669960d6a9ea8545c0f
wangwei <wwfortunate@gmail.com>
2021-03-24 10:33:29 +0800

    减少文件修改，避免nacos监听文件变更时可能读取到不正确节点信息
    Change-Id: Ib86c5be69bcf46d47bf62cc0742e8dea7850872e

--- plugin/peer/on-change.sh
+++ plugin/peer/on-change.sh
@@ -7,15 +7,15 @@ while read -ra LINE; do
     PEERS=("${PEERS[@]}" $LINE)
 done
 
-echo "" > "${CLUSTER_CONF}"
-
 if [ ${#PEERS[@]} -eq 1 ]; then
 
     echo "${PEERS[0]}:$NACOS_APPLICATION_PORT" > "${CLUSTER_CONF}"
     exit
 fi
+CONTENT=""
 for peer in "${PEERS[@]}"; do
- echo "${peer}:$NACOS_APPLICATION_PORT" >> "${CLUSTER_CONF}"
+ CONTENT="${CONTENT}${peer}:$NACOS_APPLICATION_PORT\n"
 done
 
-echo "on change write peers:"${PEERS}
+echo -e "${CONTENT%\\n}" > "${CLUSTER_CONF}"
+echo "on change write peers:"${PEERS[@]}
